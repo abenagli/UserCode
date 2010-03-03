da78f8d1b642bf5550ba7de2146fa473b45d468f
abenagli <>
2010-03-03 13:23:53 +0000

    updates

--- VBFAnalysisPackage/scripts/setup.csh
+++ VBFAnalysisPackage/scripts/setup.csh
@@ -1,5 +1,10 @@
 setenv THISDIR `pwd`
 
 setenv LD_LIBRARY_PATH ${LD_LIBRARY_PATH}:${THISDIR}/lib
+setenv LD_LIBRARY_PATH ${LD_LIBRARY_PATH}:${THISDIR}/../NtuplePackage/lib
+
 #setenv DYLD_LIBRARY_PATH ${DYLD_LIBRARY_PATH}:${THISDIR}/lib
+
 setenv PATH ${PATH}:${THISDIR}/bin
+setenv PATH ${PATH}:${THISDIR}/../NtuplePackage/bin
+
--- VBFAnalysisPackage/scripts/setup.sh
+++ VBFAnalysisPackage/scripts/setup.sh
@@ -1,5 +1,10 @@
 export THISDIR=`pwd`
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${THISDIR}/lib
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${THISDIR}/../NtuplePackage/lib
+
 #export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${THISDIR}/lib
+
 export PATH=${PATH}:${THISDIR}/bin
+export PATH=${PATH}:${THISDIR}/../NtuplePackage/bin
+
