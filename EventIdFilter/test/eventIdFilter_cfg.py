51933d617e4bf31650ae31ef132a7559a041676d
abenagli <>
2010-04-14 19:42:09 +0000

    updates

--- EventIdFilter/test/eventIdFilter_cfg.py
+++ EventIdFilter/test/eventIdFilter_cfg.py
@@ -5,7 +5,7 @@ process = cms.Process("EventIdFilter")
 
 # initialize MessageLogger and output report
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(100)
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1)
 process.MessageLogger.cerr.threshold = 'INFO'
 
 
@@ -14,7 +14,7 @@ process.source = cms.Source(
     "PoolSource",
     skipEvents = cms.untracked.uint32(0),                       
     fileNames = cms.untracked.vstring(
-        '/store/data/Commissioning10/MinimumBias/RAW-RECO/v7/000/132/440/FCCA95EA-4A3C-DF11-B108-00E0817918BF.root'
+        'file:/gwtera5/users/data/RECO/B647CAD9-0E3C-DF11-886F-0030487CD716.root'
     )
 )
 
@@ -26,26 +26,21 @@ process.maxEvents = cms.untracked.PSet(
 # output
 process.out = cms.OutputModule(
     "PoolOutputModule",
-    process.FEVTEventContent,
     verbose = cms.untracked.bool(True),
-    fileName = cms.untracked.string('Run132440_ev1.root'),
+    outputCommands = cms.untracked.vstring('keep *_*_*_*'),
+    fileName = cms.untracked.string('run132440_ev3950385.root')
     )
-
-process.out.outputCommands.extend(cms.untracked.vstring('keep *_*_*_TEST'))
 
 
 # Event id filter
 from CommonTools.EventIdFilter.eventIdFilter_cfi import *
 process.myEventIdFilter = eventIdFilter.clone()
 process.myEventIdFilter.runId = cms.int32(132440)
-process.myEventIdFilter.eventId = cms.int32(1)
+process.myEventIdFilter.eventId = cms.int32(3950385)
 
 
 # Paths
 process.p = cms.Path(
-    process.myEventIdFilter
-    )
-
-process.o = cms.OutPath(
+    process.myEventIdFilter *
     process.out
     )
