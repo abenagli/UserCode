2cdd0d42b62ee1fbbb7fc100e1ae7e9b88c0d41e
abenagli <>
2011-02-15 17:37:43 +0000

    updates for macosx

--- Fall10/NtuplePackage/scripts/setup.sh
+++ Fall10/NtuplePackage/scripts/setup.sh
@@ -4,7 +4,10 @@ else
 export THISDIR=`pwd`
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${THISDIR}/lib
+
+if [ -n "${DYLD_LIBRARY_PATH}" ]; then
 export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${THISDIR}/lib
+fi
 
 export PATH=${PATH}:${THISDIR}/bin
 
