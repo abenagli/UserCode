8e85fcd818a69f952e9f5133c83f98783df89011
abenagli <>
2011-02-15 11:56:47 +0000

    first import

---
+++ Fall10/VBFAnalysisPackage/scripts/VBFPreselection/lancia.sh
@@ -0,0 +1,2 @@
+qsub -V -q production -d /gwpool/users/benaglia/COLLISIONS7TeV/Fall10/VBFAnalysisPackage/scripts/VBFPreselection/VBF_HToWWToLNuQQ_M-170_7TeV-powheg-pythia6_Fall10-START38_V12-v1_AK5PF/JOB_1 /gwpool/users/benaglia/COLLISIONS7TeV/Fall10/VBFAnalysisPackage/scripts/VBFPreselection/VBF_HToWWToLNuQQ_M-170_7TeV-powheg-pythia6_Fall10-START38_V12-v1_AK5PF/JOB_1/bjob_1.sh
+
