6bb946c880a2a27f795034b8f03f4cf8e21d9be1
abenagli <>
2011-06-09 14:38:13 +0000

    various updates

--- Fall10/VBFAnalysisPackage/bkgstudies/higgsMassFit/higgsMassFit.C
+++ Fall10/VBFAnalysisPackage/bkgstudies/higgsMassFit/higgsMassFit.C
@@ -19,42 +19,16 @@
 
 
 
-void higgsMassFit(const std::string& mode = "exclusion", const bool& drawPlots = false)
+void SetXSignal(double& xMin_signal, double& xMax_signal, const int& mH);
+
+
+
+void higgsMassFit(const int& mH, const std::string& mode = "exclusion", const bool& drawPlots = false, const int& nToys = 10000)
 {
   using namespace RooFit;
   RooMsgService::instance().deleteStream(0);
   RooMsgService::instance().deleteStream(1);
   
-  
-  
-  //-------------------
-  // define the outfile
-  TFile* outFile = new TFile("higgsMassFit_H400.root","RECREATE");
-  outFile -> cd();
-  
-  
-  
-  //-------------------
-  // define the infiles
-  
-  std::string BKGPath = "/grid_mnt/vol__vol1__u/llr/cms/abenagli/COLLISIONS7TeV/Fall10/VBFAnalysisPackage/data/VBFAnalysis_AK5PF_H400_ET30_maxDeta_minDeta_Spring11_EGMu_noHiggsMassCut/";
-  
-  std::string WJetsFolder = "WJetsToLNu_TuneZ2_7TeV-madgraph-tauola_Spring11-PU_S1_START311_V1G1-v1/";
-  std::string TTJetsFolder = "TTJets_TuneZ2_7TeV-madgraph-tauola_Spring11-PU_S1_START311_V1G1-v1/";
-  //std::string ZJetsFolder = 
-  //std::string GJets_HT40To100Folder 
-  //std::string GJets_HT100To200Folder 
-  //std::string GJets_HT200Folder 
-  //std::string WWFolder 
-  //std::string WZFolder 
-  //std::string TJets_schannelFolder 
-  //std::string TJets_tchannelFolder 
-  //std::string TJets_tWchannelFolder 
-  
-  std::string GluGluHToLNuQQFolder = "GluGluToHToWWToLNuQQ_M-400_7TeV-powheg-pythia6_Spring11-PU_S1_START311_V1G1-v1/";
-  std::string GluGluHToTauNuQQFolder = "GluGluToHToWWToTauNuQQ_M-400_7TeV-powheg-pythia6_Spring11-PU_S1_START311_V1G1-v1/";
-  std::string VBFHToLNuQQFolder = "VBF_HToWWToLNuQQ_M-400_7TeV-powheg-pythia6_Spring11-PU_S1_START311_V1G1-v1/";
-  std::string VBFHToTauNuQQFolder = "VBF_HToWWToTauNuQQ_M-400_7TeV-powheg-pythia6_Spring11-PU_S1_START311_V1G1-v1/";
   
   
   std::string varName = "lepNuW_m";
@@ -68,10 +42,56 @@ void higgsMassFit(const std::string& mode = "exclusion", const bool& drawPlots =
   int nBins = 50;
   double xMin = 0.;
   double xMax = 1000.;
+  double xMin_signal = 0.;
+  double xMax_signal = 0.;
+  SetXSignal(xMin_signal,xMax_signal,mH);
+  
   RooRealVar x("x",varName.c_str(),xMin,xMax);
+  x.setRange("low",   xMin,       xMin_signal);
+  x.setRange("signal",xMin_signal,xMax_signal);
+  x.setRange("high",  xMax_signal,xMax);
+  
   RooRealVar w("w","weight",0.,1000000000.);
   
+  char signalCut[50];
+  sprintf(signalCut,"x > %f && x < %f",xMin_signal,xMax_signal);
   
+  
+  
+  //-------------------
+  // define the outfile
+  char outFileName[50];
+  sprintf(outFileName,"higgsMassFit_H%d_%s.root",mH,mode.c_str());
+  TFile* outFile = new TFile(outFileName,"RECREATE");
+  outFile -> cd();
+  
+  
+  
+  //-------------------
+  // define the infiles
+  char higgsMass[50];
+  sprintf(higgsMass,"%d",mH);
+  
+  std::string BKGPath = "/grid_mnt/vol__vol1__u/llr/cms/abenagli/COLLISIONS7TeV/Fall10/VBFAnalysisPackage/data/VBFAnalysis_AK5PF_H" + 
+                        std::string(higgsMass) + 
+                        "_ET30_maxDeta_minDeta_Spring11_EGMu_noHiggsMassCut/";
+  
+  std::string WJetsFolder = "WJetsToLNu_TuneZ2_7TeV-madgraph-tauola_Spring11-PU_S1_START311_V1G1-v1/";
+  std::string TTJetsFolder = "TTJets_TuneZ2_7TeV-madgraph-tauola_Spring11-PU_S1_START311_V1G1-v1/";
+  //std::string ZJetsFolder = 
+  //std::string GJets_HT40To100Folder 
+  //std::string GJets_HT100To200Folder 
+  //std::string GJets_HT200Folder 
+  //std::string WWFolder 
+  //std::string WZFolder 
+  //std::string TJets_schannelFolder 
+  //std::string TJets_tchannelFolder 
+  //std::string TJets_tWchannelFolder 
+  
+  std::string GluGluHToLNuQQFolder   = "GluGluToHToWWToLNuQQ_M-"   + std::string(higgsMass) + "_7TeV-powheg-pythia6_Spring11-PU_S1_START311_V1G1-v1/";
+  std::string GluGluHToTauNuQQFolder = "GluGluToHToWWToTauNuQQ_M-" + std::string(higgsMass) + "_7TeV-powheg-pythia6_Spring11-PU_S1_START311_V1G1-v1/";
+  std::string VBFHToLNuQQFolder      = "VBF_HToWWToLNuQQ_M-"       + std::string(higgsMass) + "_7TeV-powheg-pythia6_Spring11-PU_S1_START311_V1G1-v1/";
+  std::string VBFHToTauNuQQFolder    = "VBF_HToWWToTauNuQQ_M-"     + std::string(higgsMass) + "_7TeV-powheg-pythia6_Spring11-PU_S1_START311_V1G1-v1/";
   
   
   
@@ -116,8 +136,8 @@ void higgsMassFit(const std::string& mode = "exclusion", const bool& drawPlots =
   
   
   
-  //------------------
-  // define the signal
+  //-----------------------------------
+  // define the signal shape histograms
   int nSIG = 2;
   
   TH1F* SIGShapeHisto = new TH1F("SIGShapeHisto","",4*nBins,xMin,xMax);
@@ -233,9 +253,13 @@ void higgsMassFit(const std::string& mode = "exclusion", const bool& drawPlots =
   if( drawPlots )
   {
     TCanvas* c1 = new TCanvas("BKGShapeStack","BKGShapeStack");
+    c1 -> SetGridx();
+    c1 -> SetGridy();
     BKGShapeStack -> Draw("HIST");
     SIGShapeHisto -> Draw("HIST,same");  
-    c1 -> Print("BKGShapeStack.png","png");
+    char pngFileName[50];
+    sprintf(pngFileName,"BKGShapeStack_H%d_%s.png",mH,mode.c_str());
+    c1 -> Print(pngFileName,"png");
   }  
   
   
@@ -288,6 +312,8 @@ void higgsMassFit(const std::string& mode = "exclusion", const bool& drawPlots =
   if( drawPlots )
   {
     TCanvas* c2 = new TCanvas("rooTotPdf","rooTotPdf");
+    c2 -> SetGridx();
+    c2 -> SetGridy();
     
     RooPlot* rooBKGPlot = x.frame();
     rooBKGTotPdf -> plotOn(rooBKGPlot,LineColor(kBlack));
@@ -303,7 +329,10 @@ void higgsMassFit(const std::string& mode = "exclusion", const bool& drawPlots =
     TH1F* BKGShapeHistoNorm = (TH1F*) BKGTotShapeHisto -> Clone();
     BKGShapeHistoNorm -> Scale(1./BKGTotShapeHisto->Integral()/nBKG);
     BKGShapeHistoNorm -> Draw("HIST,same");
-    c2 -> Print("BKGShapeHistoNorm.png","png");
+
+    char pngFileName[50];
+    sprintf(pngFileName,"BKGShapeNorm_H%d_%s.png",mH,mode.c_str());
+    c2 -> Print(pngFileName,"png");
   }
   
   
@@ -324,20 +353,18 @@ void higgsMassFit(const std::string& mode = "exclusion", const bool& drawPlots =
   RooDataSet* rooSIGToyDataSet = rooSIGPdf->generate(RooArgSet(x),NSIGToy);
   rooBKGToyDataSet -> append(*rooSIGToyDataSet);
   
-  x.setRange("low",     0., 330.);
-  x.setRange("signal",330., 480.);
-  x.setRange("high",  480.,1000.);
-  
-  float NBKGToy_signal = rooBKGToyDataSet->sumEntries("x>330. && x<480.");
+  float NBKGToy_signal = rooBKGToyDataSet->sumEntries(signalCut);
   float NBKGToy_signal_fit = 0.;
   
   
   
+  // fit
   if( mode == "exclusion" ) rooTotPdf -> fitTo(*rooBKGToyDataSet,Extended(kTRUE),PrintLevel(-1),Range("low,high"));
   if( mode == "discovery" ) rooTotPdf -> fitTo(*rooBKGToyDataSet,Extended(kTRUE),PrintLevel(-1));
   
   
   
+  // count events
   if( mode == "exclusion" )
   {
     RooAbsReal* rooTotIntegral = rooTotPdf -> createIntegral(x,NormSet(x),Range("signal"));
@@ -358,20 +385,25 @@ void higgsMassFit(const std::string& mode = "exclusion", const bool& drawPlots =
   if( drawPlots )
   {
     TCanvas* c3 = new TCanvas("TOY","TOY");
+    c3 -> SetGridx();
+    c3 -> SetGridy();
     
     RooPlot* rooTOYPlot = x.frame();
     rooBKGToyDataSet -> plotOn(rooTOYPlot);
     rooTotPdf -> plotOn(rooTOYPlot, LineColor(kRed));
     rooTotPdf -> plotOn(rooTOYPlot, Components("rooSIGPdf"), LineColor(kRed));
     rooTOYPlot->Draw();
-    c3 -> Print("TOY.png","png");
+    
+    char pngFileName[50];
+    sprintf(pngFileName,"BKGToyFit_H%d_%s.png",mH,mode.c_str());
+    c3 -> Print(pngFileName,"png");
   }
   
   
   
   
   
-  /*
+  
   //-------------------------
   // generate toy experiments
   
@@ -380,37 +412,46 @@ void higgsMassFit(const std::string& mode = "exclusion", const bool& drawPlots =
   
   TRandom3 B;
   TRandom3 S;
-  for(int j = 0; j < 10000; ++j)
+  for(int j = 0; j < nToys; ++j)
   {
     if( j%100 == 0 )
       std::cout << ">>>>>> generating toy experiment " << j << std::endl;
     
     NBKGToy = B.Poisson(BKGTotShapeHisto->Integral());
-    //NSIGToy = S.Poisson(SIGShapeHisto->Integral());
     NSIGToy = 0;
+    if( mode == "discovery" ) NSIGToy = S.Poisson(SIGShapeHisto->Integral());
     
     rooBKGToyDataSet = rooBKGTotPdf->generate(RooArgSet(x),NBKGToy);
     rooSIGToyDataSet = rooSIGPdf->generate(RooArgSet(x),NSIGToy);
     rooBKGToyDataSet -> append(*rooSIGToyDataSet);
     
-    //NBKGToy_signal = rooBKGToyDataSet->sumEntries("x>330. && x<480.");
-    //NSIGToy_signal = rooSIGToyDataSet->sumEntries("x>330. && x<480.");
+    NBKGToy_signal = rooBKGToyDataSet->sumEntries(signalCut);
+    NBKGToy_signal_fit = 0.;
     
     
-    //rooTotPdf -> fitTo(*rooBKGToyDataSet,Extended(kTRUE),PrintLevel(-1),Verbose(false),Range("low,high"));
-    rooTotPdf -> fitTo(*rooBKGToyDataSet,Extended(kTRUE),PrintLevel(-1),Verbose(false));
+    
+    // fit
+    if( mode == "exclusion" ) rooTotPdf -> fitTo(*rooBKGToyDataSet,Extended(kTRUE),PrintLevel(-1),Range("low,high"));
+    if( mode == "discovery" ) rooTotPdf -> fitTo(*rooBKGToyDataSet,Extended(kTRUE),PrintLevel(-1));    
     
     
-    //NBKGToy_fit = 0;
-    //for(int i = 0; i < nBKG; ++i)
-    //{
-    //  RooAbsReal* rooBKGIntegral = rooBKGPdf[i] -> createIntegral(x,NormSet(x),Range("signal"));
-    //  NBKGToy_fit += rooBKGIntegral->getVal() * rooNBKGTot->getVal();
-    //}
     
+    // count events
+    if( mode == "exclusion" )
+    {
+      RooAbsReal* rooTotIntegral = rooTotPdf -> createIntegral(x,NormSet(x),Range("signal"));
+      NBKGToy_signal_fit = rooTotIntegral->getVal() * rooNBKGTot->getVal();
+      
+      h_BKGRes -> Fill(NBKGToy_signal_fit - NBKGToy_signal);
+      h_SIGRes -> Fill(0.);
+    }
     
-    h_BKGRes -> Fill(rooNBKGTot->getVal() - NBKGToy);
-    h_SIGRes -> Fill(rooNSIG->getVal() - NSIGToy);
+    if( mode == "discovery" )
+    {
+      h_BKGRes -> Fill(rooNBKGTot->getVal() - NBKGToy);
+      h_SIGRes -> Fill(rooNSIG->getVal() - NSIGToy);
+    }
+    
   }
   
   
@@ -420,5 +461,44 @@ void higgsMassFit(const std::string& mode = "exclusion", const bool& drawPlots =
   h_BKGRes -> Write();
   h_SIGRes -> Write();
   
-  outFile -> Close();*/
+  outFile -> Close();
+}
+
+
+
+
+
+
+void SetXSignal(double& xMin_signal, double& xMax_signal, const int& mH)
+{
+  if(mH == 250)
+  {
+    xMin_signal = 200.;
+    xMax_signal = 350.;
+  }
+  
+  else if(mH == 300)
+  {
+    xMin_signal = 220.;
+    xMax_signal = 420.;
+  }
+  
+  else if(mH == 400)
+  {
+    xMin_signal = 320.;
+    xMax_signal = 520.;
+  }
+  
+  else if(mH == 500)
+  {
+    xMin_signal = 400.;
+    xMax_signal = 650.;
+  }
+  
+  else if(mH == 600)
+  {
+    xMin_signal = 450.;
+    xMax_signal = 800.;
+  }
+
 }
