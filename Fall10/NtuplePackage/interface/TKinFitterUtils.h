7aa6670c230c016b808118ff4f93f2e923f31525
abenagli <>
2011-07-29 10:00:48 +0000

    first import

---
+++ Fall10/NtuplePackage/interface/TKinFitterUtils.h
@@ -0,0 +1,19 @@
+#ifndef TKinFitterUtils_h
+#define TKinFitterUtils_h
+
+#include <iostream>
+#include <cmath>
+
+
+
+double ErrEt (double Et, double Eta); 
+double ErrEta(double Et, double Eta); 
+double ErrPhi(double Et, double Eta);
+
+bool electronResolution(const double et, const double eta, double& etRes, double& etaRes, double& phiRes);
+bool muonResolution    (const double et, const double eta, double& etRes, double& etaRes, double& phiRes);
+bool PFMETResolution   (const double et, double& etRes, double& etaRes, double& phiRes);
+
+inline double square(const double x) {return x*x;}
+
+#endif
