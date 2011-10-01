fcb080b41bbf98773f83582cfc0523042d8ba91f
govoni <>
2011-10-01 20:35:48 +0000

    tolto un cout

--- Fall10/VBFAnalysisPackage/interface/histoFunc.h
+++ Fall10/VBFAnalysisPackage/interface/histoFunc.h
@@ -3,15 +3,9 @@
 
 #include "TH1.h"
 
-
-
-
-
-
 class histoFunc
 {
  public:
-  
   
   //! ctor
   histoFunc(TH1F* histo)
@@ -19,11 +13,9 @@ class histoFunc
     histo_p = histo;
   };
   
-  
   //! dtor
   ~histoFunc()
   {};
-  
   
   //! operator()
   double operator()(double* x, double* par)
@@ -33,11 +25,7 @@ class histoFunc
     double xMin = histo_p -> GetBinCenter(1);
     double xMax = histo_p -> GetBinCenter(histo_p -> GetNbinsX());
     
-    
-    
-    if( (xx < xMin) || (xx >= xMax) )
-      return 1.e-10;
-    
+    if( (xx < xMin) || (xx >= xMax) ) return 1.e-10;    
     else
     {  
       int bin = histo_p -> FindBin(xx);
@@ -56,32 +44,23 @@ class histoFunc
         bin2 = bin;
       }
       
-      
       double x1 = histo_p -> GetBinCenter(bin1);
       double y1 = histo_p -> GetBinContent(bin1);
-      
       double x2 = histo_p -> GetBinCenter(bin2);
       double y2 = histo_p -> GetBinContent(bin2);
-      
       double m = 1. * (y2 - y1) / (x2 - x1);
       
-      
-      
-      if( (y1 + m * (xx - x1)) < 1.e-10)
-        return 1.e-10;
-      
-      
+      if( (y1 + m * (xx - x1)) < 1.e-10) return 1.e-10;
       return par[0] * par[1] * (y1 + m * (xx - x1));
     }
     
     return 1.e-10;  
   }
   
-  
-  
  private:
   
-  TH1F* histo_p;
+  TH1F histo_f ;
+  TH1F* histo_p ;
 };
 
 #endif
