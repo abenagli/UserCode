f7de128695b71efec4b79589d6d47d34ba5b05e1
govoni <>
2011-10-27 15:41:57 +0000

    corrections from Giovanni

--- Fall10/VBFAnalysisPackage/macros/makeWorkspace.C
+++ Fall10/VBFAnalysisPackage/macros/makeWorkspace.C
@@ -1,4 +1,4 @@
-#include "interface/RooFitFunctions.h"
+#include "../interface/RooFitFunctions.h"
 
 int makeWorkspace () 
 {
@@ -7,13 +7,13 @@ int makeWorkspace ()
   //PG the fit variable
   //PG --- --- --- --- --- --- --- --- --- ---
 
+  x.setBins (80) ;
   w.import (x) ;
   
   //PG the background parameters and shape
   //PG --- --- --- --- --- --- --- --- --- ---
 
-  w.import (N1) ;
-  w.import (N2) ;
+  w.import (N) ;
   w.import (L1) ;
   w.import (L2) ;
   w.import (RFdoubleExp) ;
@@ -21,15 +21,17 @@ int makeWorkspace ()
   //PG the signal shape
   //PG --- --- --- --- --- --- --- --- --- ---
 
-  TFile * sigIn = new TFile ("signalShapes.root") ;
+  TFile * sigIn = new TFile ("../signalShapes.root") ;
   RooHistPdf * signal = (RooHistPdf *) sigIn->Get ("PDF_400") ;
   w.import (*signal) ;
 
   //PG the data
   //PG --- --- --- --- --- --- --- --- --- ---
 
-  TFile * bkgIn = new TFile ("testRooFit_004.root") ;
+  TFile * bkgIn = new TFile ("../testRooFit_004.root") ;
   RooDataSet * data_DS = (RooDataSet *) bkgIn->Get ("data_obs") ;
+  RooDataHist * data_binned = new RooDataHist ("data_binned", "data_binned", *data_DS->get (), *data_DS) ;
+  w.import (*data_binned) ;
   w.import (*data_DS) ;
 
   
