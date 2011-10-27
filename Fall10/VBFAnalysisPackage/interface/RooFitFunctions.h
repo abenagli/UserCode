d82755957b134c4e4bc8d08d5aef7f725b255dec
govoni <>
2011-10-27 15:43:09 +0000

    chage vars

--- Fall10/VBFAnalysisPackage/interface/RooFitFunctions.h
+++ Fall10/VBFAnalysisPackage/interface/RooFitFunctions.h
@@ -7,17 +7,16 @@
 
 
 
-RooRealVar x ("x", "higgs mass", 0., 1000.) ;
+RooRealVar x ("x", "higgs mass", 200., 1000.) ;
 
-RooRealVar N1 ("N1", "rel amplitude first exp",  0., 1.) ;
-RooRealVar N2 ("N2", "rel amplitude second exp", 0., 1.) ;
+RooRealVar N ("N", "rel amplitude second exp", 0., 1.) ;
 RooRealVar L1 ("L1", "rel attentuation first exp",  0., 1.) ;
 RooRealVar L2 ("L2", "rel attentuation first exp",  0., 1.) ;
 
 RooGenericPdf RFdoubleExp ("RFdoubleExp", "double exponential", 
 //                           "N1 * exp (-1 * L1 * x) + N2 * exp (-1 * L2 * x)",
-                           "@1 * exp (-1 * @2 * @0) + @3 * exp (-1 * @4 * @0)",
-                           RooArgSet (x, N1, L1, N2, L2)) ;
+                           "exp (-1 * @1 * @0) + @2 * exp (-1 * @3 * @0)",
+                           RooArgSet (x, L1, N, L2)) ;
                            
 #endif                           
 
