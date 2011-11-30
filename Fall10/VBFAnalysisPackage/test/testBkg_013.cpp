dbf0d46f9292c26a5976ec0316f28ff45072cb0a
govoni <>
2011-11-30 12:20:53 +0000

    variable binning size choice adjusted

--- Fall10/VBFAnalysisPackage/test/testBkg_013.cpp
+++ Fall10/VBFAnalysisPackage/test/testBkg_013.cpp
@@ -131,6 +131,16 @@ int main (int argc, char** argv)
   std::string outputRootFullFileName = "testBkg_013.root" ;
   TString m4_VAR = "lepNuW_m_KF" ;
 
+  hColl m4_EvenHigher ("m4_EvenHigher", nBins, m4_min, m4_max) ;
+  hColl m4_upper      ("m4_upper",      nBins, m4_min, m4_max) ;
+  hColl m4_upper_c    ("m4_upper_c",    nBins, m4_min, m4_max) ; // close
+  hColl m4_upper_a    ("m4_upper_a",    nBins, m4_min, m4_max) ; // away
+  hColl m4_signal     ("m4_signal",     nBins, m4_min, m4_max) ;
+  hColl m4_lower      ("m4_lower",      nBins, m4_min, m4_max) ;
+  hColl m4_lower_c    ("m4_lower_c",    nBins, m4_min, m4_max) ; // close
+  hColl m4_lower_a    ("m4_lower_a",    nBins, m4_min, m4_max) ; // away
+  hColl m4_sideband   ("m4_sideband",   nBins, m4_min, m4_max) ;
+
   TH1F * m4_EvenHigher_DATA = new TH1F ("m4_EvenHigher_DATA", "m4_EvenHigher_DATA", nBins, m4_min, m4_max) ;
   TH1F * m4_upper_DATA      = new TH1F ("m4_upper_DATA",      "m4_upper_DATA",      nBins, m4_min, m4_max) ;
   TH1F * m4_upper_c_DATA    = new TH1F ("m4_upper_c_DATA",    "m4_upper_c_DATA",    nBins, m4_min, m4_max) ;
@@ -157,14 +167,35 @@ int main (int argc, char** argv)
        ++iColl)
     {
       TCut cutLower = generalCut && lower ;
+      TCut cutLowerExtended = Form ("(%s) * 1./totEvents * crossSection * %f * PURescaleFactor(PUit_n) * eventWeight", cutLower.GetTitle (), LUMI) ;    
+
       TCut cutLower_c = generalCut && lower_c ;
+      TCut cutLower_cExtended = Form ("(%s) * 1./totEvents * crossSection * %f * PURescaleFactor(PUit_n) * eventWeight", cutLower_c.GetTitle (), LUMI) ;    
+
       TCut cutLower_a = generalCut && lower_a ;
+      TCut cutLower_aExtended = Form ("(%s) * 1./totEvents * crossSection * %f * PURescaleFactor(PUit_n) * eventWeight", cutLower_a.GetTitle (), LUMI) ;    
+
       TCut cutSignal = generalCut && signal ;
+      TCut cutSignalExtended = Form ("(%s) * 1./totEvents * crossSection * %f * PURescaleFactor(PUit_n) * eventWeight", cutSignal.GetTitle (), LUMI) ;    
+
       TCut cutUpper = generalCut && upper ;
+      TCut cutUpperExtended = Form ("(%s) * 1./totEvents * crossSection * %f * PURescaleFactor(PUit_n) * eventWeight", cutUpper.GetTitle (), LUMI) ;    
+
       TCut cutUpper_c = generalCut && upper_c ;
+      TCut cutUpper_cExtended = Form ("(%s) * 1./totEvents * crossSection * %f * PURescaleFactor(PUit_n) * eventWeight", cutUpper_c.GetTitle (), LUMI) ;    
+
       TCut cutUpper_a = generalCut && upper_a ;
+      TCut cutUpper_aExtended = Form ("(%s) * 1./totEvents * crossSection * %f * PURescaleFactor(PUit_n) * eventWeight", cutUpper_a.GetTitle (), LUMI) ;    
+
       TCut cutEvenHigher = generalCut && EvenHigher ;
+      TCut cutEvenHigherExtended = Form ("(%s) * 1./totEvents * crossSection * %f * PURescaleFactor(PUit_n) * eventWeight", cutEvenHigher.GetTitle (), LUMI) ;    
+
       TCut cutSideband = generalCut && (upper || lower) ;
+      TCut cutSidebandExtended = Form ("(%s) * 1./totEvents * crossSection * %f * PURescaleFactor(PUit_n) * eventWeight", cutSideband.GetTitle (), LUMI) ;    
+
+      cout << " reading " << iColl->first << endl ;
+      if (iColl->first.find ("ggH") != string::npos || iColl->first.find ("qqH") != string::npos) continue ;
+      
       
       if (iColl->first == "DATA")
         {
@@ -180,25 +211,63 @@ int main (int argc, char** argv)
         
           continue ;
         }   
+      TH1F * h_m4_lower = m4_lower.addSample (iColl->first.c_str ()) ;
+      TH1F * h_m4_lower_c = m4_lower_c.addSample (iColl->first.c_str ()) ;
+      TH1F * h_m4_lower_a = m4_lower_a.addSample (iColl->first.c_str ()) ;
+      TH1F * h_m4_signal = m4_signal.addSample (iColl->first.c_str ()) ;
+      TH1F * h_m4_upper = m4_upper.addSample (iColl->first.c_str ()) ;
+      TH1F * h_m4_upper_c = m4_upper_c.addSample (iColl->first.c_str ()) ;
+      TH1F * h_m4_upper_a = m4_upper_a.addSample (iColl->first.c_str ()) ;
+      TH1F * h_m4_EvenHigher = m4_EvenHigher.addSample (iColl->first.c_str ()) ;
+      TH1F * h_m4_sideband = m4_sideband.addSample (iColl->first.c_str ()) ;
+
+      TChain * chain = iColl->second ;
+      VBFAnalysisVariables vars ;
+      SetVBFPreselectionTreeBranches (vars, chain) ;
+
+      iColl->second->Draw (m4_VAR + TString (" >> ") + h_m4_lower->GetName (), cutLowerExtended) ;
+      iColl->second->Draw (m4_VAR + TString (" >> ") + h_m4_lower_c->GetName (), cutLower_cExtended) ;
+      iColl->second->Draw (m4_VAR + TString (" >> ") + h_m4_lower_a->GetName (), cutLower_aExtended) ;
+      iColl->second->Draw (m4_VAR + TString (" >> ") + h_m4_signal->GetName (), cutSignalExtended) ;
+      iColl->second->Draw (m4_VAR + TString (" >> ") + h_m4_upper->GetName (), cutUpperExtended) ;
+      iColl->second->Draw (m4_VAR + TString (" >> ") + h_m4_upper_c->GetName (), cutUpper_cExtended) ;
+      iColl->second->Draw (m4_VAR + TString (" >> ") + h_m4_upper_a->GetName (), cutUpper_aExtended) ;
+      iColl->second->Draw (m4_VAR + TString (" >> ") + h_m4_EvenHigher->GetName (), cutEvenHigherExtended) ;
+      iColl->second->Draw (m4_VAR + TString (" >> ") + h_m4_sideband->GetName (), cutSidebandExtended) ;
+
       ++index ;   
     } //PG loop over samples
 
+
+  //PG get the Wjets histograms from collections
+  TH1F * m4_upper_c_Wjet = m4_upper_c.findHisto ("m4_upper_c_Wjet") ;
+  TH1F * m4_upper_a_Wjet = m4_upper_a.findHisto ("m4_upper_a_Wjet") ;
+  TH1F * m4_signal_Wjet = m4_signal.findHisto ("m4_signal_Wjet") ;
+
   vector<double> xbins ;
   int enough = true ;
-  int eventsInBin = 0 ;
+  int eventsInBin_upper_c = 0 ;
+  int eventsInBin_upper_a = 0 ;
+  int eventsInBin_signal = 0 ;
   //PG loop on the bins of the histogram
-  for (int iBin = 1 ; iBin < m4_signal_DATA->GetNbinsX () ; ++iBin)
+  for (int iBin = 1 ; iBin < m4_signal_Wjet->GetNbinsX () ; ++iBin)
     {
-      if (enough) xbins.push_back (m4_signal_DATA->GetBinLowEdge (iBin)) ;
-      eventsInBin += m4_signal_DATA->GetBinContent (iBin) ;
-      if (eventsInBin < minEvents)
+      if (enough) xbins.push_back (m4_signal_Wjet->GetBinLowEdge (iBin)) ;
+      eventsInBin_upper_c += m4_upper_c_Wjet->GetBinContent (iBin) ;
+      eventsInBin_upper_a += m4_upper_a_Wjet->GetBinContent (iBin) ;
+      eventsInBin_signal += m4_signal_Wjet->GetBinContent (iBin) ;
+      if (eventsInBin_upper_c < minEvents ||
+          eventsInBin_upper_a < minEvents ||
+          eventsInBin_signal < minEvents)
         {
           enough = false ;
         }
       else
         {
           enough = true ;
-          eventsInBin = 0 ;
+          eventsInBin_upper_c = 0 ;
+          eventsInBin_upper_a = 0 ;
+          eventsInBin_signal  = 0 ;
         }        
     } //PG loop on the bins of the histogram
 
