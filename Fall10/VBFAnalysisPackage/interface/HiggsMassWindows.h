32216ebb5797d591e6610a8c7c226f09f200aa69
abenagli <>
2012-01-09 17:19:48 +0000

    updates

--- Fall10/VBFAnalysisPackage/interface/HiggsMassWindows.h
+++ Fall10/VBFAnalysisPackage/interface/HiggsMassWindows.h
@@ -2,16 +2,23 @@
 #define HiggsMassWindows_h
 
 #include <string>
+#include <vector>
 
 
+
+float GetBinWidth();
+
+std::vector<int> GetMasses();
+
+float GetHiggsWidth(const float& mH);
 
 float GetLepNuWMMIN(const float& mH);
 float GetLepNuWMMAX(const float& mH);
 
-float GetXFitMIN1(const float& mH, const std::string& method = "");
-float GetXFitMAX1(const float& mH, const std::string& method = "");
+float GetXFitMIN1(const float& mH, const std::string& fitMethod = "");
+float GetXFitMAX1(const float& mH, const std::string& fitMethod = "");
 
-float GetXFitMIN2(const float& mH, const std::string& method = "");
-float GetXFitMAX2(const float& mH, const std::string& method = "");
+float GetXFitMIN2(const float& mH, const std::string& fitMethod = "");
+float GetXFitMAX2(const float& mH, const std::string& fitMethod = "");
 
 #endif
