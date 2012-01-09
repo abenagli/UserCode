5ac6a6262d449a5ea8485355c15d464892e44899
abenagli <>
2012-01-09 17:56:30 +0000

    added mH=200

--- Fall10/VBFAnalysisPackage/src/HiggsMassWindows.cc
+++ Fall10/VBFAnalysisPackage/src/HiggsMassWindows.cc
@@ -2,8 +2,57 @@
 
 
 
+float GetBinWidth()
+{
+  return 2.5;
+}
+
+
+
+std::vector<int> GetMasses()
+{
+  std::vector<int> masses;
+  
+  masses.push_back(200);
+  masses.push_back(250);
+  masses.push_back(300);
+  masses.push_back(350);
+  masses.push_back(400);
+  masses.push_back(450);
+  masses.push_back(500);
+  masses.push_back(550);
+  masses.push_back(600);
+  
+  return masses;
+}
+
+
+
+
+
+
+float GetHiggsWidth(const float& mH)
+{
+  if     ( mH == 200. ) return 1.43;
+  if     ( mH == 250. ) return 4.04;
+  else if( mH == 300. ) return 8.43;
+  else if( mH == 350. ) return 15.2;
+  else if( mH == 400. ) return 29.2;
+  else if( mH == 450. ) return 47.0;
+  else if( mH == 500. ) return 68.0;
+  else if( mH == 550. ) return 93.2;
+  else if( mH == 600. ) return 123.;
+  else return -1.;
+}
+
+
+
+
+
+
 float GetLepNuWMMIN(const float& mH)
 {
+  if     ( mH == 200. ) return 180.;
   if     ( mH == 250. ) return 225.;
   else if( mH == 300. ) return 270.;
   else if( mH == 350. ) return 310.;
@@ -17,6 +66,7 @@ float GetLepNuWMMIN(const float& mH)
 
 float GetLepNuWMMAX(const float& mH)
 {
+  if     ( mH == 200. ) return 220.;
   if     ( mH == 250. ) return 275.;
   else if( mH == 300. ) return 330.;
   else if( mH == 350. ) return 390.;
@@ -33,75 +83,98 @@ float GetLepNuWMMAX(const float& mH)
 
 
 
-float GetXFitMIN1(const float& mH, const std::string& method)
+float GetXFitMIN1(const float& mH, const std::string& fitMethod)
 {
-  if( method == "highMass" )
+  if( (fitMethod == "doubleExponential") || (fitMethod == "doubleExponentialNoHoles") )
   {
-    if     ( mH == 250. ) return 275.;
-    else if( mH == 300. ) return 275.;
-    else if( mH == 350. ) return 275.;
-    else if( mH == 400. ) return 275.;
-    else if( mH == 450. ) return 275.;
-    else if( mH == 500. ) return 275.;
-    else if( mH == 550. ) return 275.;
-    else if( mH == 600. ) return 275.;
-    else return -1.;    
+    if     ( mH == 200. ) return 225.;
+    if     ( mH == 250. ) return 225.;
+    else if( mH == 300. ) return 225.;
+    else if( mH == 350. ) return 225.;
+    else if( mH == 400. ) return 225.;
+    else if( mH == 450. ) return 225.;
+    else if( mH == 500. ) return 225.;
+    else if( mH == 550. ) return 225.;
+    else if( mH == 600. ) return 225.;
+    else return 1.;
+  }
+  
+  else if( (fitMethod == "attenuatedDoubleExponential") || (fitMethod == "attenuatedDoubleExponentialNoHoles") )
+  {
+    if     ( mH == 200. ) return 170.;
+    if     ( mH == 250. ) return 170.;
+    else if( mH == 300. ) return 170.;
+    else if( mH == 350. ) return 170.;
+    else if( mH == 400. ) return 170.;
+    else if( mH == 450. ) return 170.;
+    else if( mH == 500. ) return 170.;
+    else if( mH == 550. ) return 170.;
+    else if( mH == 600. ) return 170.;
+    else return 1.;
+  }
+  
+  else return -1.;
+}
+
+float GetXFitMAX1(const float& mH, const std::string& fitMethod)
+{
+  if( (fitMethod == "doubleExponentialNoHoles") || (fitMethod == "attenuatedDoubleExponentialNoHoles") )
+  {
+    return -1.;
   }
   
   else
   {
-    if     ( mH == 250. ) return 200.;
-    else if( mH == 300. ) return 200.;
-    else if( mH == 350. ) return 200.;
-    else if( mH == 400. ) return 200.;
-    else if( mH == 450. ) return 200.;
-    else if( mH == 500. ) return 200.;
-    else if( mH == 550. ) return 200.;
-    else if( mH == 600. ) return 200.;
+    if     ( mH == 200. ) return GetLepNuWMMIN(200.);
+    if     ( mH == 250. ) return GetLepNuWMMIN(250.);
+    else if( mH == 300. ) return GetLepNuWMMIN(300.);
+    else if( mH == 350. ) return GetLepNuWMMIN(350.);
+    else if( mH == 400. ) return GetLepNuWMMIN(400.);
+    else if( mH == 450. ) return GetLepNuWMMIN(450.);
+    else if( mH == 500. ) return GetLepNuWMMIN(500.);
+    else if( mH == 550. ) return GetLepNuWMMIN(550.);
+    else if( mH == 600. ) return GetLepNuWMMIN(600.);
     else return -1.;
   }
 }
 
-float GetXFitMAX1(const float& mH, const std::string& method)
+
+
+
+
+
+float GetXFitMIN2(const float& mH, const std::string& fitMethod)
 {
-  if     ( mH == 250. ) return GetLepNuWMMIN(250.);
-  else if( mH == 300. ) return GetLepNuWMMIN(300.);
-  else if( mH == 350. ) return GetLepNuWMMIN(350.);
-  else if( mH == 400. ) return GetLepNuWMMIN(400.);
-  else if( mH == 450. ) return GetLepNuWMMIN(450.);
-  else if( mH == 500. ) return GetLepNuWMMIN(500.);
-  else if( mH == 550. ) return GetLepNuWMMIN(550.);
-  else if( mH == 600. ) return GetLepNuWMMIN(600.);
-  else return -1.;
+  if( (fitMethod == "doubleExponentialNoHoles") || (fitMethod == "attenuatedDoubleExponentialNoHoles") )
+  {
+    return -1.;
+  }
+  
+  else
+  {
+    if     ( mH == 200. ) return GetLepNuWMMAX(200.);
+    if     ( mH == 250. ) return GetLepNuWMMAX(250.);
+    else if( mH == 300. ) return GetLepNuWMMAX(300.);
+    else if( mH == 350. ) return GetLepNuWMMAX(350.);
+    else if( mH == 400. ) return GetLepNuWMMAX(400.);
+    else if( mH == 450. ) return GetLepNuWMMAX(450.);
+    else if( mH == 500. ) return GetLepNuWMMAX(500.);
+    else if( mH == 550. ) return GetLepNuWMMAX(550.);
+    else if( mH == 600. ) return GetLepNuWMMAX(600.);
+    else return -1.;
+  }
 }
 
-
-
-
-
-
-float GetXFitMIN2(const float& mH, const std::string& method)
+float GetXFitMAX2(const float& mH, const std::string& fitMethod)
 {
-  if     ( mH == 250. ) return GetLepNuWMMAX(250.);
-  else if( mH == 300. ) return GetLepNuWMMAX(300.);
-  else if( mH == 350. ) return GetLepNuWMMAX(350.);
-  else if( mH == 400. ) return GetLepNuWMMAX(400.);
-  else if( mH == 450. ) return GetLepNuWMMAX(450.);
-  else if( mH == 500. ) return GetLepNuWMMAX(500.);
-  else if( mH == 550. ) return GetLepNuWMMAX(550.);
-  else if( mH == 600. ) return GetLepNuWMMAX(600.);
-  else return -1.;
-}
-
-float GetXFitMAX2(const float& mH, const std::string& method)
-{
-  if     ( mH == 250. ) return 1000.;
-  else if( mH == 300. ) return 1000.;
-  else if( mH == 350. ) return 1000.;
-  else if( mH == 400. ) return 1000.;
-  else if( mH == 450. ) return 1000.;
-  else if( mH == 500. ) return 1000.;
-  else if( mH == 550. ) return 1000.;
-  else if( mH == 600. ) return 1000.;
+  if     ( mH == 200. ) return 800.;
+  if     ( mH == 250. ) return 800.;
+  else if( mH == 300. ) return 800.;
+  else if( mH == 350. ) return 800.;
+  else if( mH == 400. ) return 800.;
+  else if( mH == 450. ) return 800.;
+  else if( mH == 500. ) return 800.;
+  else if( mH == 550. ) return 800.;
+  else if( mH == 600. ) return 800.;
   else return -1.;
 }
