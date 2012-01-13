52638b549a6a2b75dccf46ade4c80c57f2a9caa8
govoni <>
2012-01-13 14:12:50 +0000

    for roofit functions

--- Fall10/VBFAnalysisPackage/interface/LinkDef.h
+++ Fall10/VBFAnalysisPackage/interface/LinkDef.h
@@ -1,7 +1,13 @@
-#ifndef ROOT_TREE_VECTOR_LINKDEFF_H 
-#define ROOT_TREE_VECTOR_LINKDEFF_H 1
+#ifndef ROOT_FUNCS_LINKDEFF_H 
+#define ROOT_FUNCS_LINKDEFF_H 1
+
+#include "SuperCrystalBall.h"
+#include "RooGaussWithSkirt.h"
 
 #ifdef __CINT__
+
+#pragma link C++ class RooGaussWithSkirt+;
+#pragma link C++ class SuperCrystalBall+;
 
 #endif
 
