1168fd4f6224baef660b5912e058a8f5e3ddaa1d
dimatteo <>
2012-01-24 16:55:03 +0000

    updated

--- Fall10/VBFAnalysisPackage/scripts/VBFHLTAnalysis/clear.sh
+++ Fall10/VBFAnalysisPackage/scripts/VBFHLTAnalysis/clear.sh
@@ -1,2 +1,13 @@
+cp launchJobs_hercules.sh ../
+cp clear.sh ../
+cp PFMETto_template.cfg ../
 rm *.sh.e*
 rm *.sh.o*
+rm *.cfg
+rm *.root
+rm *.sh
+rm *~
+rm out_*.txt
+cp ../launchJobs_hercules.sh ./
+cp ../PFMETto_template.cfg ./
+cp ../clear.sh ./
