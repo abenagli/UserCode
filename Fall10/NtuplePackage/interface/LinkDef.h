8a2fd4b91b89b71542b8366b86137aaed42f08dd
abenagli <>
2012-01-27 10:13:59 +0000

    updates

--- Fall10/NtuplePackage/interface/LinkDef.h
+++ Fall10/NtuplePackage/interface/LinkDef.h
@@ -2,6 +2,9 @@
 #define ROOT_TREE_VECTOR_LINKDEF_H 1
 
 #include "Math/Vector4D.h"
+#include "RooSuperCrystalBall.h"
+#include "RooAttenuatedCrystalBallLowHigh.h"
+#include "RooGaussWithSkirt.h"
 
 #ifdef __CINT__
 
@@ -10,12 +13,15 @@
 
 #pragma link C++ class std::vector<float>;
 #pragma link C++ class std::vector<Long64_t>;
+#pragma link C++ class RooGaussWithSkirt;
+#pragma link C++ class RooSuperCrystalBall;
+#pragma link C++ class RooAttenuatedCrystalBallLowHigh;
 #pragma link C++ function deltaEta (const double&, const double&);
 #pragma link C++ function deltaPhi (const double&, const double&);
 #pragma link C++ function deltaR (const double&, const double&, const double&, const double&);
 #pragma link C++ function PURescaleFactor (const int&, const int&, const int&, const int&);
 
-#endif
 
 #endif
 
+#endif
