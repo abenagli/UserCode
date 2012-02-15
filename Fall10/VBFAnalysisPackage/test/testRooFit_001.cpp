a0753a8214f95572e4d6b06b325dc667ecdc1805
govoni <>
2012-02-15 09:22:26 +0000

    bugfix ovvio

--- Fall10/VBFAnalysisPackage/test/testRooFit_001.cpp
+++ Fall10/VBFAnalysisPackage/test/testRooFit_001.cpp
@@ -40,6 +40,17 @@ using namespace std ;
 
 int main (int argc, char** argv)
 {
+
+//  mu = new RooRealVar("mu","",tempMu,tempMu,tempMu);
+//  kT = new RooRealVar("kT","",tempKT,tempKT,tempKT);
+//  N  = new RooRealVar("N", "",tempN,0.,1000000.0);
+//  L1 = new RooRealVar("L1","",tempL1,0.,0.1);
+//  L2 = new RooRealVar("L2","",tempL2,0.,0.1);
+//
+//  pdf_bkg = new RooGenericPdf("bkg","","1./(exp(-1.*(@0-@1)/@2)+1.) * (exp(-1*@4*@0) + @3*exp(-1*@5*@0))",RooArgSet(*x,*mu,*kT,*N,*L1,*L2));
+
+
+
   //RooRealVar x ("x", "higgs mass", 200., 1000.) ;
   //int xBinning = 160 ; //PG 5 GeV binning
   //int xBinning = 80 ; //PG 10 GeV binning
