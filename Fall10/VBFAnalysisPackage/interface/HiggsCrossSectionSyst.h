4689f8f782dcccae461c6a2ed4aa7dac9c045add
abenagli <>
2012-02-20 15:17:50 +0000

    updates

--- Fall10/VBFAnalysisPackage/interface/HiggsCrossSectionSyst.h
+++ Fall10/VBFAnalysisPackage/interface/HiggsCrossSectionSyst.h
@@ -2,9 +2,16 @@
 #define HiggsCrossSectionSyst_h
 
 #include <string>
+#include <cmath>
 
 
 
 double HiggsCrossSectionSyst(const float& mH, const std::string& productionMode, const std::string& updown);
+
+double HiggsQCDScaleSyst(const float& mH, const std::string& productionMode, const std::string& updown);
+
+double HiggsPDFSyst(const float& mH, const std::string& productionMode, const std::string& updown);
+
+double HiggsHighMassSyst(const float& mH, const std::string& productionMode, const std::string& updown);
 
 #endif
