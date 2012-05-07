1cf58e2a987c4c2c65e5e2b9fa0634494ce30caa
dimatteo <>
2012-05-07 12:29:20 +0000

    updated syst

--- Fall10/VBFAnalysisPackage/src/FitBiasSyst.cc
+++ Fall10/VBFAnalysisPackage/src/FitBiasSyst.cc
@@ -6,31 +6,29 @@ double FitBiasSyst(const float& mH, const std::string& flavour)
 {
   if( flavour == "e" )
   {
-    //return 0.228;
-    
-    if     ( mH == 250 ) return 0.083;
-    else if( mH == 300 ) return 0.242;
-    else if( mH == 350 ) return 0.202;
-    else if( mH == 400 ) return 0.108;
-    else if( mH == 450 ) return 0.061;
+    if     ( mH == 200 ) return 0.127;
+    if     ( mH == 250 ) return 0.170;
+    else if( mH == 300 ) return 0.135;
+    else if( mH == 350 ) return 0.045;
+    else if( mH == 400 ) return 0.045;
+    else if( mH == 450 ) return 0.143;
     else if( mH == 500 ) return 0.132;
-    else if( mH == 550 ) return 0.341;
-    else if( mH == 600 ) return 0.656;
+    else if( mH == 550 ) return 0.241;
+    else if( mH == 600 ) return 0.383;
     else return 0.;
   }
   
   if( flavour == "mu" )
   {
-    //return 0.117;
-    
-    if     ( mH == 250 ) return 0.098;
-    else if( mH == 300 ) return 0.088;
-    else if( mH == 350 ) return 0.075;
-    else if( mH == 400 ) return 0.046;
+    if     ( mH == 200 ) return 0.030;
+    if     ( mH == 250 ) return 0.042;
+    else if( mH == 300 ) return 0.026;
+    else if( mH == 350 ) return 0.046;
+    else if( mH == 400 ) return 0.032;
     else if( mH == 450 ) return 0.053;
-    else if( mH == 500 ) return 0.100;
-    else if( mH == 550 ) return 0.193;
-    else if( mH == 600 ) return 0.281;
+    else if( mH == 500 ) return 0.021;
+    else if( mH == 550 ) return 0.103;
+    else if( mH == 600 ) return 0.180;
     else return 0.;
   }
   
