a81204e870d114247be4544d03de66e66ff877e9
abenagli <>
2013-06-10 14:20:27 +0000

    updates

--- ECALStudies/CommonUtils/scripts/setup_lxplus.sh
+++ ECALStudies/CommonUtils/scripts/setup_lxplus.sh
@@ -11,7 +11,6 @@ export THISDIR=`pwd`
 
 export LD_LIBRARY_PATH=${THISDIR}/lib:${LD_LIBRARY_PATH}
 #export DYLD_LIBRARY_PATH=${THISDIR}/lib:${DYLD_LIBRARY_PATH}
-
 export PATH=${THISDIR}/bin:${PATH}
 
 export COMMONUTILS=${THISDIR}/
