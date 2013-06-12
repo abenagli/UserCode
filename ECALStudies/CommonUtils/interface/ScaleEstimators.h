2341b8b8d9be94865c1481551691c0ff5f173f71
abenagli <>
2013-06-12 09:12:21 +0000

    updates

--- ECALStudies/CommonUtils/interface/ScaleEstimators.h
+++ ECALStudies/CommonUtils/interface/ScaleEstimators.h
@@ -16,26 +16,26 @@
 
 void FindSmallestInterval(double& mean, double& meanErr, double& min, double& max,
                           std::vector<double>& vals, std::vector<double>& weights,
-                          const double& fraction, const bool& verbosity);
+                          const double& fraction, const bool& verbosity = false);
 
 void FindRecursiveMean(double& mean, double& meanErr,
                        std::vector<double>& vals, std::vector<double>& weights,
                        const double& window, const double& tolerance,
-                       const bool& verbosity);
+                       const bool& verbosity = false);
 
 void FindGausFit(double& mean, double& meanErr,
                  std::vector<double>& vals, std::vector<double>& weights,
                  const int& nBins, const double& xMin, const double& xMax,
                  TF1** f_gausFit, const std::string& name,
                  const double& startingMean,
-                 const bool& verbosity);
+                 const bool& verbosity = false);
 
 void FindMean(double& mean, double& meanErr,
               std::vector<double>& vals, std::vector<double>& weights,
-              const bool& verbosity);
+              const bool& verbosity = false);
 
 void FindTemplateFit(double& scale, double& scaleErr,
                      TH1F* h_MC, TH1F* h_DA,
-                     const bool& verbosity);
+                     const bool& verbosity = false);
 
 #endif
